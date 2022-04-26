78274038ba6daa2b226085054b0e8377d956ca13
Aakash Shukla <shukla.aa@northeastern.edu>
2022-04-26 18:19:35 -0400

    Made corrections to reference errors in insertion scripts

--- Scripts/8-createSystemRoles.sql
+++ Scripts/8-createSystemRoles.sql
@@ -2,6 +2,7 @@ create or replace procedure createRoles(roleName in varchar)
 is roleId number default ROLEMASTERSEQUENCE.nextval;
 begin
     insert into rolemaster values (roleId, upper(roleName));
+    COMMIT;
 end;
 /
 
--- Scripts/9-createUsers.sql
+++ Scripts/9-createUsers.sql
@@ -12,17 +12,18 @@ SPECC in VARCHAR2,
 EMAIL in VARCHAR2,
 PSWD in VARCHAR2,
 ROLE_ID in NUMBER,
-IS_ACTIVE in BINARY_DOUBLE
+IS_ACTIVE in CHAR
 )
 
 is 
 empId number default EMPLOYEESEQUENCE.nextval;
 begin 
    insert into employee values (empId, FNAME, LNAME, ADDRESS_1, ADDRESS_2, CITY, STATE, ZIPCODE, PHONE, SPECC, EMAIL, PSWD, ROLE_ID, IS_ACTIVE);
+   COMMIT;
 end;
 /
 
-create or replace PROCEDURE patientdetails
+create or replace PROCEDURE createPatient
 (
 FNAME in VARCHAR2,
 LNAME in VARCHAR2,
@@ -42,6 +43,7 @@ is
 patientId number default PATIENTSEQUENCE.nextval;
 begin 
    insert into patientdetails values (patientId, FNAME, LNAME, GENDER, ADDRESS_1, ADDRESS_2, CITY, STATE, ZIPCODE, PHONE, EMAIL, PSWD, DOB);
+   COMMIT;
 end;
 /
 
@@ -62,28 +64,30 @@ is
 billId number default BILLSEQUENCE.nextval;
 begin 
    insert into bill values (billId, DOCTORFEE, PHARMACYCHARGE, ROOMCHARGE, OPTCHARGE, NOOFDAYS, MISCCHARGE, LABCHARGE, INSURANCENUMBER, APPOINTMENTID);
+   COMMIT;
 end;
 /
 
-create or replace PROCEDURE bookappointment
+create or replace PROCEDURE createAppointment
 (
 	PATIENTID in NUMBER,
-       APPOINTMENTNUMBER in VARCHAR2,
-       APPOINTMENTTYPE in VARCHAR2,
-       REQUESTDATE in  DATE,
-       APPOINTMENTTIME in DATE,
-       DOCTORID in  NUMBER,
-       APPOINTMENTDESCRIPTION in VARCHAR2
+    APPOINTMENTNUMBER in VARCHAR2,
+    APPOINTMENTTYPE in VARCHAR2,
+    REQUESTDATE in  DATE,
+    APPOINTMENTTIME in DATE,
+    DOCTORID in  NUMBER,
+    APPOINTMENTDESCRIPTION in VARCHAR2
 )
 
 is 
 appointmentId number default appointmentSequence.nextval;
 begin 
-   insert into bill values (appointmentId, DOCTORFEE, PHARMACYCHARGE, ROOMCHARGE, OPTCHARGE, NOOFDAYS, MISCCHARGE, LABCHARGE, INSURANCENUMBER, APPOINTMENTID);
+   insert into appointment values (appointmentId,PATIENTID, APPOINTMENTNUMBER, APPOINTMENTTYPE, REQUESTDATE, APPOINTMENTTIME, DOCTORID, APPOINTMENTDESCRIPTION);
+   COMMIT;
 end;
 /
 
-create or replace PROCEDURE insuranceDetails
+create or replace PROCEDURE addInsuranceDetails
 (
 	PATIENTID in NUMBER, 
 	STARTDATE in DATE, 
@@ -97,26 +101,27 @@ create or replace PROCEDURE insuranceDetails
 is 
 id number default insuranceSequence.nextval;
 begin 
-   insert into bill values (id, PATIENTID , STARTDATE, ENDDATE, OPTICAL, DENTAL, MATERNITY, INSURANCECODE);
+   insert into insurance values (id, PATIENTID , STARTDATE, ENDDATE, OPTICAL, DENTAL, MATERNITY, INSURANCECODE);
+   COMMIT;
 end;
 /
 
-create or replace PROCEDURE insuranceMasterdetails
+create or replace PROCEDURE addMasterInsuranceData
 (
-	PATIENTID in VARCHAR2, 
 	PLAN in VARCHAR2, 
 	COPAY in NUMBER, 
-	COVERAGE in VARCHAR2,
+	COVERAGE in VARCHAR2
 )
 
 is 
 INSURANCECODE number default insurancemasterSequence.nextval;
 begin 
-   insert into bill values (INSURANCECODE, PATIENTID , PLAN, COPAY, COVERAGE);
+   insert into insuranceMaster values (INSURANCECODE, PLAN, COPAY, COVERAGE);
+   COMMIT;
 end;
 /
 
-create or replace PROCEDURE labDetails
+create or replace PROCEDURE scheduleTest
 (
 	EMPLOYEEID in NUMBER, 
 	APPOINTMENTID in NUMBER, 
@@ -132,11 +137,12 @@ create or replace PROCEDURE labDetails
 is 
 LABNUMBER number default labSequence.nextval;
 begin 
-   insert into bill values (LABNUMBER, EMPLOYEEID , APPOINTMENTID, TESTID, HEIGHT, WEIGHT, BP, TEMPERATURE, TESTDATE, TESTRESULT);
+   insert into Lab values (LABNUMBER, EMPLOYEEID , APPOINTMENTID, TESTID, HEIGHT, WEIGHT, BP, TEMPERATURE, TESTDATE, TESTRESULT);
+   COMMIT;
 end;
 /
 
-create or replace PROCEDURE payrollDetails
+create or replace PROCEDURE addPayrollDetails
 (
 	EMP_ID in NUMBER, 
 	SAL in FLOAT, 
@@ -147,11 +153,12 @@ create or replace PROCEDURE payrollDetails
 is 
 id number default payrollSequence.nextval;
 begin 
-   insert into bill values (id, EMP_ID, SAL , BONUS, ACC_NO);
+   insert into payroll values (id, EMP_ID, SAL , BONUS, ACC_NO);
+   COMMIT;
 end;
 /
 
-create or replace PROCEDURE pharmacyMasterDetails
+create or replace PROCEDURE addPharmacyData
 (
 	MEDID in NUMBER, 
 	COMPANY in VARCHAR2, 
@@ -164,37 +171,22 @@ create or replace PROCEDURE pharmacyMasterDetails
 is 
 ID number default pharmacyMasterSequence.nextval;
 begin 
-   insert into bill values (ID, MEDID , COMPANY, QUANTITY, PRODUCEDDATE, EXPIRYDATE, SUPPLIERID);
+   insert into pharmacyMaster values (ID, MEDID , COMPANY, QUANTITY, PRODUCEDDATE, EXPIRYDATE, SUPPLIERID);
+   COMMIT;
 end;
 /
 
-create or replace PROCEDURE pharmacyMasterDetails
-(
-	MEDID in NUMBER, 
-	COMPANY in VARCHAR2, 
-	QUANTITY in NUMBER, 
-	PRODUCEDDATE in DATE, 
-	EXPIRYDATE in DATE, 
-	SUPPLIERID in NUMBER
-)
-
-is 
-ID number default pharmacyMasterSequence.nextval;
-begin 
-   insert into bill values (ID, MEDID , COMPANY, QUANTITY, PRODUCEDDATE, EXPIRYDATE, SUPPLIERID);
-end;
-/
-
-create or replace PROCEDURE prescriptionDetails
+create or replace PROCEDURE addPrescription
 (
 	REPORTID in NUMBER, 
-	PHARMACYID in NUMBER,
+	PHARMACYID in NUMBER
 )
 
 is 
 ID number default prescriptionSequence.nextval;
 begin 
-   insert into bill values (ID, REPORTID , PHARMACYID);
+   insert into prescription values (ID, REPORTID , PHARMACYID);
+   COMMIT;
 end;
 /
 
@@ -208,23 +200,12 @@ create or replace PROCEDURE createReport
 is 
 REPORTID number default reportSequence.nextval;
 begin 
-   insert into bill values (REPORTID, APPOINTMENTID , DIAGNOSIS, DOCTORID);
+   insert into report values (REPORTID, APPOINTMENTID , DIAGNOSIS, DOCTORID);
+   COMMIT;
 end;
 /
 
-create or replace PROCEDURE roleMasterDetails
-(
-	ROLENAME in VARCHAR2,
-)
-
-is 
-ROLEID number default roleMasterSequence.nextval;
-begin 
-   insert into bill values (ROLENAME);
-end;
-/
-
-create or replace PROCEDURE roomDetails
+create or replace PROCEDURE addRoomData
 (
 	CATEGORY in VARCHAR2, 
 	STATUS in VARCHAR2, 
@@ -233,11 +214,12 @@ create or replace PROCEDURE roomDetails
 is 
 ROOM_ID number default roomSequence.nextval;
 begin 
-   insert into bill values (ROOM_ID, CATEGORY , STATUS, BEDCOUNT);
+   insert into room values (ROOM_ID, CATEGORY , STATUS, BEDCOUNT);
+   commit;
 end;
 /
 
-create or replace PROCEDURE supplierMasterDetails
+create or replace PROCEDURE addSupplierData
 (
 	NAME in VARCHAR2, 
 	PHONE in VARCHAR2, 
@@ -251,7 +233,8 @@ create or replace PROCEDURE supplierMasterDetails
 is 
 SUPPLIERID number default supplierSequence.nextval;
 begin 
-   insert into bill values (SUPPLIERID, NAME, PHONE, EMAIL, ADDRESS_1, ADDRESS_2, CITY, STATE, ZIPCODE);
+   insert into supplierMaster values (SUPPLIERID, NAME, PHONE, EMAIL, ADDRESS_1, ADDRESS_2, CITY, STATE, ZIPCODE);
+   COMMIT;
 end;
 /
 
