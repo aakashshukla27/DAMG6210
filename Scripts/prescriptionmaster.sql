d25dab90b173865ebf97745c0467d2c1900e2093
RiyaMoitra <71420868+RiyaMoitra@users.noreply.github.com>
2022-04-27 03:55:19 -0400

    Add files via upload

---
+++ Scripts/prescriptionmaster.sql
@@ -0,0 +1,26 @@
+SELECT * From PRESCRIPTIONMASTER;
+
+insert into PRESCRIPTIONMASTER values (70,'Gabapentin',89.8);
+insert into PRESCRIPTIONMASTER values (32,'Anti-Bacterial Hand',394);
+insert into PRESCRIPTIONMASTER values (36,'Bio Heather',359);
+insert into PRESCRIPTIONMASTER values (3,'System Formula',787);
+insert into PRESCRIPTIONMASTER values (42,'equate nitetime',375);
+insert into PRESCRIPTIONMASTER values (22,'NUCYNTA',123);
+insert into PRESCRIPTIONMASTER values (60,'Air',351);
+insert into PRESCRIPTIONMASTER values (27,'Acetaminophen',594);
+insert into PRESCRIPTIONMASTER values (99,'SHISEIDO SHEER ',59.9);
+insert into PRESCRIPTIONMASTER values (68,'Stomach Relief',260);
+insert into PRESCRIPTIONMASTER values (53,'Zolpidem tartrate',747);
+insert into PRESCRIPTIONMASTER values (66,'CAMERA READY',746);
+insert into PRESCRIPTIONMASTER values (21,'VERELAN',635);
+insert into PRESCRIPTIONMASTER values (11,'Arnuity Ellipta',495);
+insert into PRESCRIPTIONMASTER values (78,'Prenate Chewable',885);
+insert into PRESCRIPTIONMASTER values (54,'OLYSIO',628);
+insert into PRESCRIPTIONMASTER values (88,'Verticalm',88.1);
+insert into PRESCRIPTIONMASTER values (10,'metolazone',347);
+insert into PRESCRIPTIONMASTER values (26,'Green Pea',453);
+insert into PRESCRIPTIONMASTER values (7,'Arth Arrest',538);
+insert into PRESCRIPTIONMASTER values (17,'Gemfibrozil',913);
+insert into PRESCRIPTIONMASTER values (31,'Pleo Not',961);
+insert into PRESCRIPTIONMASTER values (82,'Sedalia',427);
+insert into PRESCRIPTIONMASTER values (43,'Olanzapine',828);
