d67a418cd6905b0749b013561c217fe00c33bce7
Aakash Shukla <shukla.aa@northeastern.edu>
2022-04-28 16:12:28 -0400

    Added new scripts

--- Scripts/20-views.sql
+++ Scripts/20-views.sql
@@ -10,3 +10,75 @@ inner join prescriptionmaster pm on p.pharmacyId = pm.id
 
 create or replace view HRView AS
 select * from employee inner join payroll on employee."employeeid" = payroll.emp_id;
+
+
+create or replace view doctorList AS
+select * from employee where role_id = 1;
+
+
+grant select on dmddhospitaladmin.appointment to patient1;
+grant select on dmddhospitaladmin.patientdetails to patient1;
+
+grant select on dmddhospitaladmin.patientdocument to patient1;
+
+
+-- supplier master
+grant select on dmddhospitaladmin.suppliermaster to pharmacist1;
+grant insert on dmddhospitaladmin.suppliermaster to pharmacist1;
+grant update on dmddhospitaladmin.suppliermaster to pharmacist1;
+grant select on dmddhospitaladmin.suppliermaster to manager1;
+grant insert on dmddhospitaladmin.suppliermaster to manager1;
+grant update on dmddhospitaladmin.suppliermaster to manager1;
+
+-- insurance master
+grant select on dmddhospitaladmin.insurancemaster to patient1;
+grant select on dmddhospitaladmin.insurancemaster to receptionist1;
+grant select on dmddhospitaladmin.insurancemaster to manager1;
+grant insert on dmddhospitaladmin.insurancemaster to patient1;
+grant update on dmddhospitaladmin.insurancemaster to patient1;
+grant insert on dmddhospitaladmin.insurancemaster to receptionist1;
+grant update on dmddhospitaladmin.insurancemaster to receptionist1;
+
+-- room
+grant select on dmddhospitaladmin.room to doctor1;
+grant select on dmddhospitaladmin.room to nurse1;
+grant select on dmddhospitaladmin.room to receptionist1;
+grant insert on dmddhospitaladmin.room to receptionist1;
+grant update on dmddhospitaladmin.room to receptionist1;
+grant select on dmddhospitaladmin.room to manager1;
+grant insert on dmddhospitaladmin.room to manager1;
+grant update on dmddhospitaladmin.room to manager1;
+
+
+-- payroll
+grant select on dmddhospitaladmin.payroll to doctor1;
+grant select on dmddhospitaladmin.payroll to nurse1;
+grant select on dmddhospitaladmin.payroll to receptionist1;
+grant select on dmddhospitaladmin.payroll to labassistant1;
+grant select on dmddhospitaladmin.payroll to pharmacist1;
+grant select on dmddhospitaladmin.payroll to manager1;
+grant select on dmddhospitaladmin.payroll to hr1;
+
+grant select on dmddhospitaladmin.rolemaster to manager1;
+grant insert on dmddhospitaladmin.rolemaster to manager1;
+grant update on dmddhospitaladmin.rolemaster to manager1;
+
+grant select on dmddhospitaladmin.employee to manager1;
+grant insert on dmddhospitaladmin.employee to manager1;
+grant update on dmddhospitaladmin.employee to manager1;
+
+grant select on dmddhospitaladmin.rolemaster to hr1;
+grant insert on dmddhospitaladmin.rolemaster to hr1;
+grant update on dmddhospitaladmin.rolemaster to hr1;
+
+grant select on dmddhospitaladmin.employee to hr1;
+grant insert on dmddhospitaladmin.employee to hr1;
+grant update on dmddhospitaladmin.employee to hr1;
+grant select on dmddhospitaladmin.employee to patient1;
+
+grant select on dmddhospitaladmin.doctorList to patient1;
+
+
+grant select on dmddhospitaladmin.lab to labassistant1;
+grant insert on dmddhospitaladmin.lab to labassistant1;
+grant update on dmddhospitaladmin.lab to labassistant1;
--- Scripts/22-demo.sql
+++ Scripts/22-demo.sql
@@ -1,4 +1,10 @@
 -- create appointment
+grant select on dmddhospitaladmin.appointment to patient1;
+grant select on dmddhospitaladmin.patientdetails to patient1;
+
+grant select on dmddhospitaladmin.patientdocument to patient1;
+
+
 begin
     createappointment(1, 'Physical/Check Up', '16-Jan-2022','16-Jan-2022 09:00', 3, 'Marginal corneal ulcer, bilateral');
 end;
@@ -9,3 +15,5 @@ begin
 addPrescription(5, 4);
 addPrescription(5, 12);
 end;
+
+
